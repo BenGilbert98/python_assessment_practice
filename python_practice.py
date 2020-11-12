bf727d44fb56b72a046992a33d04d9c2b86efa76
BenGilbert98 <73166552+BenGilbert98@users.noreply.github.com>
2020-11-12 10:32:24 +0000

    set task

--- python_practice.py
+++ python_practice.py
@@ -1,6 +1,20 @@
 # declare a list with numbers 1-5 and add 6 to end
 
-list = [1, 2, 3, 4, 5]
-print(list)
-list.append(6)
-print(list)
+# list = [1, 2, 3, 4, 5]
+# print(list)
+# list.append(6)
+# print(list)
+
+# create a tuple 1-5
+
+my_tuple = (1, 2, 3, 4, 5)
+print(my_tuple)
+print(type(my_tuple))
+
+num_set = {1, 2, 3, 4, 5}
+
+for number in num_set:
+    if number <= 3:
+        print(number)
+    else:
+        break
